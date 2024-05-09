254d61eec3a3e00c3c620c4f247a9c34cae64a26
BronsveldT <137558061+BronsveldT@users.noreply.github.com>
2024-05-09 18:30:45 +0200

    Road asset (#1)
    * Added the initial roadAssetTransfer methods to move object to ledger
    
    * Fixed the update roadAsset to correctly add the given distance travelled
    
    * Added missing readme
    
    * First initial complete chaincode. Shud've done more commits

---
+++ README.md
@@ -0,0 +1,38 @@
+# Repository for the chaincode of the PPK project.
+
+./network.sh up createChannel -c capgemini -ca -s couchdb
+
+./network.sh deployCC -c capgemini -ccn basic -ccp ../../chaincode_ppk/ -ccl java
+
+Package the chaincode
+
+peer lifecycle chaincode package test.tar.gz --path ../../ppk_chaincode/ --lang java --label test_1.0
+
+export PATH=${PWD}/../bin:$PATH
+
+export FABRIC_CFG_PATH=$PWD/../config/
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer lifecycle chaincode install test.tar.gz
+
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+
+peer lifecycle chaincode queryinstalled
+
+export CC_PACKAGE_ID=est_1.0:6c8b3cbbc7dec732fce21066c3a170ca95fa0c1ed44fc05a274e353c296cd68e
+
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID capgemini --name test --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+
+peer lifecycle chaincode checkcommitreadiness --channelID capgemini --name test --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
+
+peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID capgemini --name test --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt"
+
+peer lifecycle chaincode querycommitted --channelID capgemini --name test --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
--- src/main/java/org/capgemini/RoadAssetTransfer.java
+++ src/main/java/org/capgemini/RoadAssetTransfer.java
@@ -1,12 +1,15 @@
 package org.capgemini;
 
-import org.hyperledger.fabric.contract.annotation.Contact;
-import org.hyperledger.fabric.contract.annotation.Contract;
-import org.hyperledger.fabric.contract.annotation.Info;
-import org.hyperledger.fabric.contract.annotation.License;
+import com.owlike.genson.Genson;
+import org.hyperledger.fabric.contract.Context;
+import com.owlike.genson.annotation.JsonProperty;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.*;
+import org.hyperledger.fabric.shim.ChaincodeException;
+import org.hyperledger.fabric.shim.ChaincodeStub;
 
 @Contract(
-        name = "driverAsset",
+        name = "basic",
         info = @Info(
                 title = "Driver Asset Transfer",
                 description = "Capgemini initial contract for driver assets",
@@ -20,5 +23,130 @@ import org.hyperledger.fabric.contract.annotation.License;
                 )
         )
 )
-public class RoadAssetTransfer {
+@Default
+public class RoadAssetTransfer implements ContractInterface {
+
+    private final Genson genson = new Genson();
+
+    private enum AssetTransferErrors {
+        ASSET_NOT_FOUND,
+        ASSET_ALREADY_EXISTS
+    }
+
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public boolean roadAssetExists(final Context ctx, final String roadId) {
+        ChaincodeStub stub = ctx.getStub();
+        String assetJSON = stub.getStringState(roadId);
+
+        return (assetJSON != null && !assetJSON.isEmpty());
+    }
+    /***
+     *
+     * Creates a new asset on the ledger
+     *
+     * @param ctx
+     * @param roadAdminType
+     * @param streetName
+     * @param adminNumber
+     * @param distanceTravelledOn
+     * @param adminName
+     * @param roadAdminName
+     * @param municipality
+     * @param state
+     * @return the created asset
+     */
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public RoadAsset createRoadAsset(final Context ctx,
+                                     final String roadId,
+                                     final String roadAdminType,
+                                     final String streetName,
+                                     final int adminNumber,
+                                     final double distanceTravelledOn,
+                                     final String adminName,
+                                     final String roadAdminName,
+                                     final String municipality,
+                                     final String state) {
+
+        ChaincodeStub stub = ctx.getStub();
+        if(roadAssetExists(ctx, roadId)){
+            String errorMessages = String.format("Asset %s already exists", roadId);
+            System.out.println(errorMessages);
+            throw new ChaincodeException(errorMessages, AssetTransferErrors.ASSET_ALREADY_EXISTS.toString());
+        }
+
+        RoadAsset roadAsset = new RoadAsset(
+                roadId,
+                roadAdminType,
+                streetName,
+                adminNumber,
+                distanceTravelledOn,
+                adminName,
+                roadAdminName,
+                municipality,
+                state
+        );
+
+        String sortedJson = genson.serialize(roadAsset);
+        stub.putStringState(roadId, sortedJson);
+        return roadAsset;
+    }
+
+    /**
+     * Retrieves an asset with the specified ID from the ledger
+     * @param ctx
+     * @param roadId
+     * @return
+     */
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public RoadAsset readRoadAsset(final Context ctx, final String roadId) {
+        ChaincodeStub stub = ctx.getStub();
+        String assetJSON = stub.getStringState(roadId);
+
+        if(assetJSON != null && !assetJSON.isEmpty()){
+            String errorMessage = String.format("Asset %s does not exist", roadId);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_NOT_FOUND.toString());
+        }
+
+        RoadAsset roadAsset = genson.deserialize(assetJSON, RoadAsset.class);
+        return roadAsset;
+    }
+
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public RoadAsset updateRoadAsset(final Context ctx,
+                                     final String roadId,
+                                     final double distanceTravelledOn) {
+
+        ChaincodeStub stub = ctx.getStub();
+
+        if(!roadAssetExists(ctx, roadId)){
+            String errorMessages = String.format("Asset %s does not exist", roadId);
+            System.out.println(errorMessages);
+            throw new ChaincodeException(errorMessages, AssetTransferErrors.ASSET_NOT_FOUND.toString());
+        }
+        String assetJSON =  stub.getStringState(roadId);
+        RoadAsset roadAsset = genson.deserialize(assetJSON, RoadAsset.class);
+        roadAsset.addDistanceTravelledOn(distanceTravelledOn);
+        String sortedJson = genson.serialize(roadAsset);
+        stub.putStringState(roadAsset.getRoadId(), sortedJson);
+        return roadAsset;
+    }
+
+    /**
+     *
+     * @param ctx
+     * @param roadId
+     */
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public void deleteRoadAsset(final Context ctx, final String roadId) {
+        ChaincodeStub stub = ctx.getStub();
+
+        if(!roadAssetExists(ctx,roadId)) {
+            String errorMessage = String.format("Road asset %s does not exist", roadId);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_NOT_FOUND.toString());
+        }
+
+        stub.delState(roadId);
+    }
 }

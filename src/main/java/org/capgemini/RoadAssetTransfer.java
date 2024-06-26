b96386c6f056ca6483a0f05e7e9738e05eae0c13
thomas <thomas@thomas.thomas>
2024-06-26 20:12:04 +0200

    Removed driverAsset from this chaincode

--- src/main/java/org/capgemini/RoadAssetTransfer.java
+++ src/main/java/org/capgemini/RoadAssetTransfer.java
@@ -11,8 +11,8 @@ import org.hyperledger.fabric.shim.ChaincodeStub;
 @Contract(
         name = "basic",
         info = @Info(
-                title = "Driver Asset Transfer",
-                description = "Capgemini initial contract for driver assets",
+                title = "Road Asset transfer",
+                description = "Capgemini initial contract for road assets",
                 version = "0.0.1-SNAPSHOT",
                 license = @License(
                         name = "Capgemini"
